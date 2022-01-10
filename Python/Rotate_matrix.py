ca7858fe201727c1d35d6714c6743dc8f6955579
Ali-Sabirov <ali.sabirov81@gmail.com>
2022-01-10 20:14:53 +0300

    Conflicts

---
+++ Python/Rotate_matrix.py
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+
+        #print(matrix)
+        n = len(matrix[0])
+        m1 = [0]*n
+        for i in range(n):
+            m1[i] = [0]*n
+        for i in range(n):
+            for j in range(n):
+                k = n-i-1
+                m1[j][k] = matrix[i][j]
+        #print(m1)   
+        for i in range(n):
+            for j in range(n):             
+                matrix[i][j] = m1[i][j]  
