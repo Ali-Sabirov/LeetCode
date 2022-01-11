50f9f5f27bbbf24d428ad82d4b743c10831ddc9d
Ali-Sabirov <ali.sabirov81@gmail.com>
2022-01-11 22:09:25 +0300

    Update Plus One 2.py

--- Python/Plus One 2.py
+++ Python/Plus One 2.py
@@ -1,12 +1,8 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         s = ''
-        n = len(digits)
-        x = 0
-        for i in range(n): s += str(digits[i])
-        x = int(s) + 1
-        s = str(x)
-        n = len(s)
+        for i in range(len(digits)): s += str(digits[i])
+        s = str(int(s) + 1)
         d = []
-        for i in range(n): d.append(int(s[i]))
-        return d  
+        for i in range(len(s)): d.append(int(s[i]))
+        return d
